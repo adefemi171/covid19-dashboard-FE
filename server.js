2da1cd243c409b8a779197f01ffa4e0e4011e5e8
adefemi171 <adefemi171@gmail.com>
2020-08-01 20:13:18 -0400

    Updated  package.json

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static('dist'));
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8081, function () {
+  console.log('Example app listening on port 8081!');
 });
